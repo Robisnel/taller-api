8bb34e0999c7a862d6052c38ba9f0beb424fb54a
Robin <robisnelpm@gmail.com>
2023-06-03 12:48:48 -0500

    cositas extra...

--- src/components/Series.jsx
+++ src/components/Series.jsx
@@ -43,6 +43,7 @@ const Series = () => {
     return (
         <div>
             <h1>Solicitud IMDb API</h1>
+            <h2>Universidad de la Costa</h2>
             <h2>Robisnel Paniza</h2>
             <button onClick={traer}>Traer series</button>
             <button onClick={siguiente}>Siguiente</button>
